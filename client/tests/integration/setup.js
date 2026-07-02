ad1c1b8288a1b8f7f8e3b3b946e5d6ce126727a8
Alex Seriy <alex.m.seriy@gmail.com>
2026-07-02 11:32:22 +0000

    Debug playright install

--- client/tests/integration/setup.js
+++ client/tests/integration/setup.js
@@ -9,11 +9,7 @@ const DIR = path.join(os.tmpdir(), 'jest_playwright_global_setup');
  * Custom Playwright setup for Jest integration.
  */
 module.exports = async () => {
-  const browserServer = await chromium.launchServer(
-    {
-      channel: 'chrome'
-    }
-  );
+  const browserServer = await chromium.launchServer({ channel: 'chrome' });
 
   // store the browser server instance so we can teardown it later
   // this global is only available in the teardown but not in TestEnvironments
